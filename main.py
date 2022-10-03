249178ed231ac3ef01044e284bfd5655573a415b
Curvu <rodrigues.filipealexandre2003@gmail.com>
2022-10-03 15:12:06 +0100

    Created main.py with some code

---
+++ main.py
@@ -0,0 +1,17 @@
+def histograma(p, a): # (fonte, alfabeto)
+    histo = {}
+    for letra in p:
+        if letra in a:
+            if letra in histo:
+                histo[letra] += 1
+            else:
+                histo[letra] = 1
+    return histo
+
+a = 'abcdefghijklmnopqrstuvwxyz'
+
+p = 'aaaaaaaabbbcccdddeeefffggghhhiii'
+
+print(histograma(p, a))
+
+# 3 - aplicar a todas as fontes
