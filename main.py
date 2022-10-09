4573bdcae0d1708060d5589d36dddc1042f2e76b
Curvu <rodrigues.filipealexandre2003@gmail.com>
2022-10-09 10:21:38 +0100

    funcao entropia melhorada agora retorna valor, apagar comentarios desnecessarios funcao histograma, (+) funcoes analyse(Image|Wav|Txt), alfabetos corrigidos tirando o do som

--- main.py
+++ main.py
@@ -3,20 +3,11 @@ import math
 import matplotlib.image as mpimg
 from scipy.io import wavfile
 
-# ex 1
+#! ex 1
 def histograma(a, p): # (alfabeto, fonte)
-    #! Versão 1 (não inicializa o dicionário com letras que não aparecem)
-    # histo = {}
-    # for letra in p:
-    #     if letra in a:
-    #         if letra in histo:
-    #             histo[letra] += 1
-    #         else:
-    #             histo[letra] = 1
-
-    #! Versão 2 (inicializa o dicionário com letras que não aparecem)
     histo = {letra: 0 for letra in a}
     for letra in p:
+        print(letra, end='')
         if letra in a and letra in histo:
             histo[letra] += 1
             
@@ -28,7 +19,7 @@ def histograma(a, p): # (alfabeto, fonte)
     plt.show()
 
 
-# ex 2
+#! ex 2
 def entropia(a, p):
     entropia = 0
 
@@ -36,51 +27,60 @@ def entropia(a, p):
         if letra in a:
             entropia += ((1/len(p)) * math.log2(1/len(p)))
 
-    # valor calculado em cima é negativo, mas nós queremos o módulo!!
-    entropia = abs(entropia)
+    return abs(entropia) # return da entropia em modulo, porque em cima é calculado com valor negativo
 
-    # mostra o valor da entropia na consola
-    print(entropia)
+#! ex 3
+def analyseImage(src):
+    a = [chr(i) for i in range(256)] # alfabeto com todos os caracters para .bmp
+
+    img = mpimg.imread(src)
+    p = [chr(pixel) for pixel in img[0]] # põe cada pixel convertido para caracter no array
+
+    histograma(a, p)
+    print(f"Entropia {src.replace('./src/', '')}: {entropia(a, p)}")
+
+def analyseWav(src):
+    #TODO: not sure mas se for este o alfabeto então não está a funcionar como deveria
+    '''a = [i/100 for i in range(-100, 100)] # alfabeto de som no intervalo [-1, 1[ para .wav'''
+
+    a = []
+
+    [temp, data] = wavfile.read(src)
+    # possivelmente depois dar delete ao temp >>> del temp 
+    p = [chr(frame) for frame in data] # põe cada frame convertido para caracter no array
+    
+    histograma(a, p)
+    print(f"Entropia {src.replace('./src/', '')}: {entropia(a, p)}")
+
+def analyseTxt(src):
+    a = [chr(i) for i in range(65, 91)] + [chr(i) for i in range(97, 123)] # alfabeto de a-zA-Z para .txt
+
+    p = open('./src/lyrics.txt', 'r').read() # string com todo o texto
+
+    histograma(a, p) # aqui vai percorrer a string como se fosse um array
+    print(f"Entropia {src.replace('./src/', '')}: {entropia(a, p)}")
 
 
+#!              ------   Main    ------
 def main():
-    # TODO: FIX ALPHABET URGENTLY
-    # a = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    # a = [chr(i) for i in range(256)]
+    '''Section for .bmp files'''
+    # landscape
+    analyseImage('./src/landscape.bmp')
+
+    # MRI
+    analyseImage('./src/MRI.bmp')
     
+    # MRIbin
+    analyseImage('./src/MRIbin.bmp')
 
-    # landscape.bmp
-    landscape = mpimg.imread('./src/landscape.bmp')
-    p1 = []
-    for pixel in landscape[1]:
-        p1.append(chr(pixel))
 
-    # MRI.bmp
-    mri = mpimg.imread('./src/MRI.bmp')
-    p2 = []
-    for pixel in mri[0]:
-        p2.append(chr(pixel))
-    
-    # MRIbin.bmp
-    mri_bin = mpimg.imread('./src/MRIbin.bmp')
-    p3 = []
-    for pixel in mri_bin[0]:
-        p3.append(chr(pixel))
+    '''Section for .wav files'''
+    # TODO: Make alphabet for wav files
+    # soundMono
+    analyseWav('./src/soundMono.wav')
 
-    # soundMono.wav
-    # alfabeto de som no intervalo [-1, 1[
-    [fs, data] = wavfile.read('./src/soundMono.wav')
-    p4 = []
-    for frame in data:
-        p4.append(chr(frame))
-
-    # histograma(a, p4)
-    # entropia(a, p1)
-
+    '''Section for .txt files'''
+    # lyrics
+    analyseTxt('./src/lyrics.txt')
 
 main()
-
-#! vvv Ignorar vvv 
-#* 3 - aplicar a todas as fontes (img, text, wav)
-#* 0 - 255 (cada elemento - 1 byte : num medio por simbolo 8 bits)
-#* estudar o quanto é que nós conseguimos comprimir (valor mais baixo que teóricamente conseguimos atingir)
