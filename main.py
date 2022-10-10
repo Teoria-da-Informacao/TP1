5c7926a0bc16ed3dfd9739123b41c7f7e2b91aea
Curvu <rodrigues.filipealexandre2003@gmail.com>
2022-10-10 14:31:17 +0100

    analyseWav edits

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from typing import Counter
 import matplotlib.pyplot as plt
 import math
 import matplotlib.image as mpimg
 from scipy.io import wavfile
+import numpy as np
 
 #! ex 1
 def histograma(a, p, src): # (alfabeto, fonte)
@@ -10,13 +12,14 @@ def histograma(a, p, src): # (alfabeto, fonte)
         if letra in a and letra in histo:
             histo[letra] += 1
 
-    print(histo) # mostra o histograma na consola como se fosse dicionário
+    '''print(histo) # mostra o histograma na consola como se fosse dicionário'''
 
     # mostra o histograma gráfico
     plt.title(src)
     plt.bar(histo.keys(), histo.values())
     
-    plt.xticks([])
+    # Esconde label x
+    # plt.xticks([])
     # plt.yticks([letra for letra in histo if histo[letra] > 0])
 
     # abre janela maximizada
@@ -26,7 +29,6 @@ def histograma(a, p, src): # (alfabeto, fonte)
     plt.show()
 
     return histo # return dicionário
-
 
 #! ex 2
 def entropia(histo, p):
@@ -47,21 +49,25 @@ def analyseImage(src):
 
     src = src.replace('./src/', '')
     histo = histograma(a, p, src)
-    print(f"Entropia {src.replace('./src/', '')}: {entropia(histo, p)}")
+    print(f"\nEntropia {src.replace('./src/', '')}: {entropia(histo, p)}\n")
 
 def analyseWav(src):
     #TODO: not sure mas se for este o alfabeto então não está a funcionar como deveria
-    '''a = [i/100 for i in range(-100, 100)] # alfabeto de som no intervalo [-1, 1[ para .wav'''
-
-    a = []
+    # a = [i/100 for i in range(-100, 100)] # alfabeto de som no intervalo [-1, 1[ para .wav
+    # np.arrange
 
     [temp, data] = wavfile.read(src)
-    # possivelmente depois dar delete ao temp >>> del temp 
-    p = [chr(frame) for frame in data] # põe cada frame convertido para caracter no array
+    data = data / np.iinfo(data.dtype).max # normaliza os dados para o intervalo [-1, 1[
+    d = (1 - (-1)) / (2**data.itemsize)
+    a = np.arange(-1, 1, d)
+    
+    print(a)
+    print('------')
+    print(data)
     
     src = src.replace('./src/', '')
-    histo = histograma(a, p, src)
-    print(f"Entropia {src.replace('./src/', '')}: {entropia(histo, p)}")
+    histo = histograma(a, data, src)
+    print(f"Entropia {src.replace('./src/', '')}: {entropia(histo, data)}")
 
 def analyseTxt(src):
     a = [chr(i) for i in range(65, 91)] + [chr(i) for i in range(97, 123)] # alfabeto de a-zA-Z para .txt
@@ -77,21 +83,21 @@ def analyseTxt(src):
 def main():
     '''Section for .bmp files'''
     # landscape
-    analyseImage('./src/landscape.bmp')
+    # analyseImage('./src/landscape.bmp')
 
     # MRI
-    analyseImage('./src/MRI.bmp')
+    # analyseImage('./src/MRI.bmp')
     
     # MRIbin
-    analyseImage('./src/MRIbin.bmp')
+    # analyseImage('./src/MRIbin.bmp')
 
     '''Section for .wav files'''
     # TODO: Make alphabet for wav files
     # soundMono
-    # analyseWav('./src/soundMono.wav')
+    analyseWav('./src/soundMono.wav')
 
     '''Section for .txt files'''
     # lyrics
-    analyseTxt('./src/lyrics.txt')
+    # analyseTxt('./src/lyrics.txt')
 
 main()
